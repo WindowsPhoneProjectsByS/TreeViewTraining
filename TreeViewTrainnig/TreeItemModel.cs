9d6e57b72ef6cecd4fe91b7c1bd92a03ddc4dc4d
Dariusz Nurzyński <Dariusz Nurzyski>
2017-04-01 20:31:52 +0200

    Działający tree view

---
+++ TreeViewTrainnig/TreeItemModel.cs
@@ -0,0 +1,65 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using WinRTXamlToolkit.Imaging;
+
+namespace TreeViewTrainnig
+{
+    public class TreeItemModel : BindableBase
+    {
+        #region Text 
+        private string _text;
+        /// <summary> 
+        /// Gets or sets the text. 
+        /// </summary> 
+        public string Text
+        {
+            get { return _text; }
+            set { this.SetProperty(ref _text, value); }
+        }
+        #endregion
+
+        #region Depth 
+        private int _depth;
+        /// <summary> 
+        /// Gets or sets the text. 
+        /// </summary> 
+        public int Depth
+        {
+            get { return _depth; }
+            set { this.SetProperty(ref _depth, value); }
+        }
+        #endregion
+
+        #region Branch 
+        private int _branch;
+        /// <summary> 
+        /// Gets or sets the text. 
+        /// </summary> 
+        public int Branch
+        {
+            get { return _branch; }
+            set { this.SetProperty(ref _branch, value); }
+        }
+        #endregion
+
+        #region Children 
+        private ObservableCollection<TreeItemModel> _children = new ObservableCollection<TreeItemModel>();
+        /// <summary> 
+        /// Gets or sets the child items. 
+        /// </summary> 
+        public ObservableCollection<TreeItemModel> Children
+        {
+            get { return _children; }
+            set { this.SetProperty(ref _children, value); }
+        }
+        #endregion
+    }
+}
---
+++ TreeViewTrainnig/TreeViewPageViewModel.cs
@@ -0,0 +1,95 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinRTXamlToolkit.Tools;
+
+namespace TreeViewTrainnig
+{
+    public class TreeViewPageViewModel : BindableBase, INotifyPropertyChanged
+    {
+        public RelayCommand<object> cmdTreeSelected { get; private set; }
+
+        private int _itemId;
+        private Random _rand = new Random();
+
+        #region TreeItems 
+        private ObservableCollection<TreeItemModel> _treeItems;
+        public ObservableCollection<TreeItemModel> TreeItems
+        {
+            get { return _treeItems; }
+            set { this.SetProperty(ref _treeItems, value); }
+        }
+        #endregion
+
+        private string _SelectedItem = string.Empty;
+        public string SelectedItem
+        {
+            get
+            {
+                return _SelectedItem;
+            }
+
+            set
+            {
+                if (_SelectedItem == value)
+                { return; }
+
+                _SelectedItem = value;
+                RaisePropertyChanged("SelectedItem");
+            }
+        }
+
+        public TreeViewPageViewModel()
+        {
+            _itemId = 1;
+            TreeItems = BuildTree(5, 5);
+            cmdTreeSelected = new RelayCommand<object>(TreeViewItemSelectionChanged);
+        }
+
+        private ObservableCollection<TreeItemModel> BuildTree(int depth, int branches)
+        {
+            var tree = new ObservableCollection<TreeItemModel>();
+
+            if (depth > 0)
+            {
+                var depthIndices = Enumerable.Range(0, branches).Shuffle();
+
+                for (int i = 0; i < branches; i++)
+                {
+                    var d = depthIndices[i] % depth;
+                    var b = _rand.Next(branches / 2, branches);
+                    tree.Add(
+                        new TreeItemModel
+                        {
+                            Branch = b,
+                            Depth = d,
+                            Text = "Item " + _itemId++,
+                            Children = BuildTree(d, b)
+                        });
+                }
+            }
+            return tree;
+        }
+
+        private void TreeViewItemSelectionChanged(object itm)
+        {
+            if (itm != null)
+            {
+                if (itm is TreeItemModel)
+                {
+                    TreeItemModel itmObj = (TreeItemModel)itm;
+
+                    if (itmObj.Depth == 0)
+                        itmObj.Branch = 0;
+
+                    SelectedItem = itmObj.Text.ToString() + " is in position " + itmObj.Depth + " having " + itmObj.Branch + " Branches.";
+                }
+            }
+        }
+    }
+}
